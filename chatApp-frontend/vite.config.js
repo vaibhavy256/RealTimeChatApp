0751098af5adf58a3b654ed79f929f47c29db31a
Vaibhav Yeotikar <92702730+vaibhavy256@users.noreply.github.com>
2024-12-30 18:37:49 +0530

    build updated

--- chatApp-frontend/vite.config.js
+++ chatApp-frontend/vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/RealTimeChatApp/",
   build:{
     outDir: "./docs",
   },
   
+
 })
